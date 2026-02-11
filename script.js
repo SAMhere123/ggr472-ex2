014549df19d8b2c75e31e887ce905d339eff658d
SAMhere123 <itss.a.m20xx@gmail.com>
2026-02-11 08:17:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+const btn = document.getElementById("my-button");
+btn.addEventListener("click", () => {
+    // alert("You clicked me!")
+    console.log("You clicked me!")
+});
+// An example
+
+// Select all rating buttons
+const buttons = document.querySelectorAll('.btn btn-outline-warning rating-star');
+const StarCountDisplay = document.getElementById('count-star');
+
+// Counter
+let countStar = 0;
+
+// Add click listener to each button
+buttons.forEach(btn => {
+    btn.addEventListener('click', () => {
+        countStar++;
+        StarCountDisplay.innerHTML = countStar;
+    });
+});
+
+// Gave up trying to figure out why the star rating isn't counting
+
 // Add a web map
 mapboxgl.accessToken = 'pk.eyJ1Ijoic2FtaGVyZTEyMyIsImEiOiJjbWtkbnFtNXAwZW9iM2Zwcjc3eWZpMjFsIn0.xMGFvUR2mK0MK7uEbzr2MQ'; // Added default public map token from my Mapbox account
 
