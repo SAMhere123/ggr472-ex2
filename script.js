8e87dd3dee4079fae59489c2adf3ac8b30abb590
SAMhere123 <itss.a.m20xx@gmail.com>
2026-02-11 06:13:34 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,71 +1,9 @@
-const btn = document.getElementById("my-button");
-btn.addEventListener("click", () => {
-    // alert("You clicked me!")
-    console.log("You clicked me!")
-});
-// An example
-
-// Select all rating buttons
-const buttons = document.querySelectorAll('.btn btn-outline-warning rating-star');
-const StarCountDisplay = document.getElementById('count-star');
-
-// Counter
-let countStar = 0;
-
-// Add click listener to each button
-buttons.forEach(btn => {
-    btn.addEventListener('click', () => {
-        countStar++;
-        StarCountDisplay.innerHTML = countStar;
-    });
-});
-
-// Gave up trying to figure out why the star rating isn't counting
-
 // Add a web map
-mapbox.accessToken = 'pk.eyJ1Ijoic2FtaGVyZTEyMyIsImEiOiJjbWtkbnFtNXAwZW9iM2Zwcjc3eWZpMjFsIn0.xMGFvUR2mK0MK7uEbzr2MQ'; // Added default public map token from my Mapbox account
+mapboxgl.accessToken = 'pk.eyJ1Ijoic2FtaGVyZTEyMyIsImEiOiJjbWtkbnFtNXAwZW9iM2Zwcjc3eWZpMjFsIn0.xMGFvUR2mK0MK7uEbzr2MQ'; // Added default public map token from my Mapbox account
 
 const map = new mapboxgl.Map({
-    container: 'my map', // map container ID
+    container: 'my-map', // map container ID
     style: 'mapbox://styles/samhere123/cmlhtwwmm004q01s369o56w8b', //style URL
-    center: [-79.39, 43.66], //starting position[lng, lat0]
-    zoom: 12, //starting zoom
+    center: [-79.43, 43.64], //starting position[lng, lat]
+    zoom: 12, //starting zoom level
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
